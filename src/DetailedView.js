ec067abc500cfe2800571611a2382e3927c7e8f6
rahmaas <ash.rahman@verizonwireless.com>
2018-08-03 01:55:31 -0500

    base styles in place

--- src/DetailedView.js
+++ src/DetailedView.js
@@ -4,12 +4,19 @@ import Map from './Map'
 const DetailedView = ({ restaurant }) => (
   <div class='detail-view'>
     <Map location={restaurant.location} />
-    <h1>{restaurant.name}</h1>
-    <h2>{restaurant.category}</h2>
-    <p>{restaurant.location.formattedAddress[0]}</p>
-    <p>{restaurant.location.formattedAddress[1]}</p>
-    <p>{restaurant.contact.formattedPhone}</p>
-    <p>{`@${restaurant.contact.twitter}`}</p>
+
+    <div class='restaurant-details detail-banner'>
+      <h1 class='restaurant-name'>{restaurant.name}</h1>
+      <h2 class='restaurant-category'>{restaurant.category}</h2>
+    </div>
+
+    <div class='detail-group'>
+      <p>{restaurant.location.formattedAddress[0]}</p>
+      <p style={{ marginBottom: 26 }}> {restaurant.location.formattedAddress[1]}</p>
+    
+      <p style={{ marginBottom: 26 }}>{restaurant.contact.formattedPhone}</p>
+      <p>{`@${restaurant.contact.twitter}`}</p>
+    </div>
   </div>
 )
 
