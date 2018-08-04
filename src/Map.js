41c08339620f117da66235f90945fcdd096d0c93
rahmaas <ash.rahman@verizonwireless.com>
2018-08-03 23:42:51 -0500

    style fixes, animation improvement

--- src/Map.js
+++ src/Map.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import scriptLoader from 'react-async-script-loader'
 
-export default class Map extends Component { 
-  componentDidMount() {
+export class Map extends Component {
+  componentWillReceiveProps({ isScriptLoadSucceed }) {
+    if(isScriptLoadSucceed) {
       this.map = new google.maps.Map(this.refs.map, {
-        center: { 
-          lat: this.props.location.lat, 
-          lng: this.props.location.lng 
+        center: {
+          lat: this.props.location.lat,
+          lng: this.props.location.lng
         },
         zoom: 16
       })
   
       let marker = new google.maps.Marker({
-        position: { 
-          lat: this.props.location.lat, 
-          lng: this.props.location.lng 
+        position: {
+          lat: this.props.location.lat,
+          lng: this.props.location.lng
         },
         map: this.map
       })
-      console.log(this.map)
+    }
   }
 
   render() {
-    return(
+    return (
       <div ref='map' style={{ width: '100%', height: 180, backgroundColor: '#e6e6e6' }}></div>
     )
   }
@@ -41,3 +43,5 @@ Map.propTypes = {
     lng: PropTypes.number
   })
 }
+
+export default scriptLoader(['https://maps.googleapis.com/maps/api/js?key=AIzaSyDkhNcFY2PRqMkp0JGgvWwdeDdM0cGaa7I'])(Map)
